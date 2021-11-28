f2a6e909401aad3898969901936620166cb4a07f
JacksonJ97 <jacksonjude97@gmail.com>
2021-11-28 00:29:15 -0500

    changed the handleLoadExample function to work with state hooks

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -127,57 +127,61 @@ const Container = () => {
     }
   };
 
-  // handleLoadExample() {
-  //   this.setState({
-  //     firstName: "John",
-  //     lastName: "Smith",
-  //     address: "123 Random Street",
-  //     phoneNumber: "123-456-7890",
-  //     email: "example@gmail.com",
-  //     summary:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  //     experience: [
-  //       {
-  //         id: uniqueId(),
-  //         company: "Example Company",
-  //         startDate: "April 2019",
-  //         endDate: "March 2020",
-  //         details: [
-  //           { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-  //           { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-  //         ],
-  //       },
-  //       {
-  //         id: uniqueId(),
-  //         company: "Example Company",
-  //         startDate: "April 2017",
-  //         endDate: "March 2019",
-  //         details: [
-  //           { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-  //           { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-  //         ],
-  //       },
-  //     ],
-  //     education: [
-  //       {
-  //         id: uniqueId(),
-  //         school: "Havard University",
-  //         degree: "Bachelor of Engineering (BEng) in Computer Engineering",
-  //         location: "Cambridge, MA",
-  //         startDate: "Sept 2020",
-  //         endDate: "April 2021",
-  //       },
-  //       {
-  //         id: uniqueId(),
-  //         school: "Ryerson University",
-  //         degree: "Bachelor of Engineering (BEng) in Electrical Engineering",
-  //         location: "Toronto, ON",
-  //         startDate: "Sept 2015",
-  //         endDate: "April 2020",
-  //       },
-  //     ],
-  //   });
-  // }
+  const handleLoadExample = () => {
+    setBasicInfo({
+      firstName: "John",
+      lastName: "Smith",
+      address: "123 Random Street",
+      phoneNumber: "123-456-7890",
+      email: "example@gmail.com",
+    });
+
+    setSummaryInfo(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    );
+
+    setExperienceInfo([
+      {
+        id: uniqueId(),
+        company: "Example Company",
+        startDate: "April 2019",
+        endDate: "March 2020",
+        details: [
+          { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+          { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+        ],
+      },
+      {
+        id: uniqueId(),
+        company: "Example Company",
+        startDate: "April 2017",
+        endDate: "March 2019",
+        details: [
+          { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+          { id: uniqueId(), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+        ],
+      },
+    ]);
+
+    setEducationInfo([
+      {
+        id: uniqueId(),
+        school: "Havard University",
+        degree: "Bachelor of Engineering (BEng) in Computer Engineering",
+        location: "Cambridge, MA",
+        startDate: "Sept 2020",
+        endDate: "April 2021",
+      },
+      {
+        id: uniqueId(),
+        school: "Ryerson University",
+        degree: "Bachelor of Engineering (BEng) in Electrical Engineering",
+        location: "Toronto, ON",
+        startDate: "Sept 2015",
+        endDate: "April 2020",
+      },
+    ]);
+  };
 
   return (
     <Wrapper>
@@ -192,7 +196,7 @@ const Container = () => {
         handleDeleteDetail={handleDeleteDetail}
         handleDelete={handleDelete}
         handleAdd={handleAdd}
-        // handleLoadExample={this.handleLoadExample}
+        handleLoadExample={handleLoadExample}
       />
       <Preview basicInfo={basicInfo} summaryInfo={summaryInfo} experienceInfo={experienceInfo} educationInfo={educationInfo} />
     </Wrapper>
